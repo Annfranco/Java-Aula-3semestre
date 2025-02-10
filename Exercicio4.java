a1981d5740369aba8380dd8481f37ef8373fe038
Annfranco <160090372+Annfranco@users.noreply.github.com>
2025-02-10 14:15:24 +0000

    Exercicio4

---
+++ Exercicio4.java
@@ -0,0 +1,8 @@
+public class Exercicio4 {
+    public static void main(String[] args) {
+        
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Valor de i: " + i);
+        }
+    }
+}

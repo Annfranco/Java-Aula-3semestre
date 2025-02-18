c97a2a2b5804ef7ee27f50796c111e6f7c07fdfb
Annfranco <160090372+Annfranco@users.noreply.github.com>
2025-02-18 12:17:49 +0000

    aula 18/02

--- encapsulamento/src/main/java/com/example/MainCliente.java
+++ encapsulamento/src/main/java/com/example/MainCliente.java
@@ -3,6 +3,7 @@ package com.example;
 public class MainCliente {
     public static void main(String[] args) {
         // instaciando classe - criando objeto
+        // Cliente 1.
         Cliente cliente1 = new Cliente();
 
         cliente1.setNome("Marta");
@@ -17,5 +18,8 @@ public class MainCliente {
         Cliente cliente2 = new Cliente("Julia", "juju@email.com", 254);
         System.out.println(cliente2.toString());
         
+        // Cliente 3.
+        Cliente cliente3 = new Cliente("Flavia", "flavia.b@email.com", 697);
+        System.out.println(cliente3.toString());
     }
 }

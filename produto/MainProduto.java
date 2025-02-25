7bd29cfb3f096fe5e64b2a3e3a1c8046712127ef
Annfranco <160090372+Annfranco@users.noreply.github.com>
2025-02-25 12:28:09 +0000

    update

--- produto/MainProduto.java
+++ produto/MainProduto.java
@@ -2,7 +2,7 @@ package produto;
 
 public class MainProduto {
     public static void main(String[] args) {
-        Processador processador1 = new Processador("Intel", "i7", "4.9GHz");
+        Processador processador1 = new Processador("Intel", "i7", "4.9G");
         Memoria memoria1 = new Memoria("Multilaser", "DDR3", "32GB");
 
         System.out.println(processador1.toString());

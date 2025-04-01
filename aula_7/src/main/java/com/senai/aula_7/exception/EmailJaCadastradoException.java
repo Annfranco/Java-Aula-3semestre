eabfe328cc5810d27456adda9bbcc96081ac9594
Annfranco <160090372+Annfranco@users.noreply.github.com>
2025-04-01 11:23:02 -0300

    Add files via upload

---
+++ aula_7/src/main/java/com/senai/aula_7/exception/EmailJaCadastradoException.java
@@ -0,0 +1,7 @@
+package com.senai.aula_7.exception;
+
+public class EmailJaCadastradoException extends RuntimeException {
+    public EmailJaCadastradoException(String message) {
+        super(message);
+    }
+}
---
+++ aula_7/src/main/java/com/senai/aula_7/repository/UsuarioRepository.java
@@ -0,0 +1,12 @@
+package com.senai.aula_7.repository;
+
+import com.senai.aula_7.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    Optional<Usuario> findByEmail(String email);
+}
